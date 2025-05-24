a6ed9cf23a1be156556ada54044842a366d3cf10
Hihiz <public@mail.ru>
2025-05-25 00:08:43 +0300

    Добавить сервис клиента микросервиса категорий

---
+++ NowBookingWeb.Category/NowBookingWeb.Category.Client/ICategoryServiceClient.cs
@@ -0,0 +1,15 @@
+﻿namespace NowBookingWeb.Category.Client
+{
+    /// <summary>
+    /// Интерфейс клиента микросервиса категорий.
+    /// </summary>
+    public interface ICategoryServiceClient
+    {
+        /// <summary>
+        /// Метод удаляет категорию.
+        /// </summary>
+        /// <param name="categoryId">Id категории.</param>
+        /// <returns>Удаленная категория.</returns>
+        Task<CategoryOutput> RemoveCategoryAsync(int categoryId, CancellationToken cancellationToken);
+    }
+}
