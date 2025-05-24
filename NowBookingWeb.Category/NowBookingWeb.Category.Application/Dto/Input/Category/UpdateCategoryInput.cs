0859122b15e615c361e664da55af238ed611f7cf
Hihiz <public@mail.ru>
2025-05-24 23:05:34 +0300

    Добавить входную модель

---
+++ NowBookingWeb.Category/NowBookingWeb.Category.Application/Dto/Input/Category/UpdateCategoryInput.cs
@@ -0,0 +1,23 @@
+﻿namespace NowBookingWeb.Category.Application.Dto.Input.Category
+{
+    /// <summary>
+    /// Класс входной модели редактирования категории.
+    /// </summary>
+    public class UpdateCategoryInput
+    {
+        /// <summary>
+        /// Id категории.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Название категории.
+        /// </summary>
+        public string? Name { get; set; }
+
+        /// <summary>
+        /// Описание категории.
+        /// </summary>
+        public string? Description { get; set; }
+    }
+}
