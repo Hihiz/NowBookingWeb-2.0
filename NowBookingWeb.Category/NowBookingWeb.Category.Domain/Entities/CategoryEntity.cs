a3445226c9c754c312cb3232bf8b3e90d7ebffdd
Hihiz <public@mail.ru>
2025-05-24 23:04:50 +0300

    Добавить сущность категории

---
+++ NowBookingWeb.Category/NowBookingWeb.Category.Domain/Entities/CategoryEntity.cs
@@ -0,0 +1,23 @@
+﻿namespace NowBookingWeb.Category.Domain.Entities
+{
+    /// <summary>
+    /// Класс сущности категории.
+    /// </summary>
+    public class CategoryEntity
+    {
+        /// <summary>
+        /// Id категории.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Название категории.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Описание категории.
+        /// </summary>
+        public string? Description { get; set; }
+    }
+}
