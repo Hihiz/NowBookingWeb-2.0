67912eaab8f67c7b545059ae8fe9edac180412b1
Hihiz <public@mail.ru>
2025-05-24 23:06:29 +0300

    Добавить интерфейс репозитория категорий

---
+++ NowBookingWeb.Category/NowBookingWeb.Category.Application/Interfaces/Repositories/ICategoryRepository.cs
@@ -0,0 +1,23 @@
+﻿using NowBookingWeb.Category.Application.Dto.Output.Category;
+
+namespace NowBookingWeb.Category.Application.Interfaces.Repositories
+{
+    /// <summary>
+    /// Интерфейс репозитория категорий.
+    /// </summary>
+    public interface ICategoryRepository
+    {
+        /// <summary>
+        /// Метод получает список категорий.
+        /// </summary>
+        /// <returns>Список категорий.</returns>
+        Task<IEnumerable<CategoryOutput>> GetCategoriesAsync();
+
+        /// <summary>
+        /// Метод удаляет категорию.
+        /// </summary>
+        /// <param name="categoryId">Id категории.</param>
+        /// <returns>Удаленная категория.</returns>
+        Task<CategoryOutput> RemoveCategoryAsync(int categoryId);
+    }
+}
